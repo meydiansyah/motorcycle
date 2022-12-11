6a38ad9b44108082bfacafc21e5bc4dd780f9bfd
meydiansyah <meydiansyah79@gmail.com>
2022-12-11 15:08:28 +0700

    Add modal login

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,16 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useAppDispatch, useAppSelector } from "@/core/redux/hooks";
+import { selectAuth } from "@/core/redux/slices/auth";
+import { setModalLogin, setModalLogout } from "@/core/redux/slices/ui/uiSlice";
+import { useState } from "react";
 import CustomButton from "./Button";
 
 const Navbar = () => {
   const router = useRouter();
+  const { token, user } = useAppSelector(selectAuth);
+  const [hover, setHover] = useState(false);
+  const dispatch = useAppDispatch();
 
   return (
     <div className="w-full fixed z-20 top-0 bg-white shadow-sm ">
@@ -41,11 +48,29 @@ const Navbar = () => {
               pathName="/articles"
             />
           </div>
-          <Link href="/login">
-            <a className="py-4 text-md cursor-pointer text-black border-b-2 border-transparent hover:border-black ease-linear transition-all duration-400">
-              Login
-            </a>
-          </Link>
+          <div
+            className="flex"
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+          >
+            {token ? (
+              <button
+                onClick={() => dispatch(setModalLogout())}
+                className={`${
+                  hover && "hover:text-red-600 hover:border-red-900"
+                } py-4 mx-4 items-center inline-flex text-black border-b-2 border-transparent ease-linear transition-all duration-400`}
+              >
+                <div className="block">{hover ? "Logout" : user.name}</div>
+              </button>
+            ) : (
+              <button
+                onClick={() => dispatch(setModalLogin())}
+                className="py-4 mx-4 items-center inline-flex text-black border-b-2 border-transparent hover:text-gray-600 hover:border-blue ease-linear transition-all duration-400"
+              >
+                <div className="block">Login</div>
+              </button>
+            )}
+          </div>
         </div>
       </div>
     </div>
--- components/layouts/App.tsx
+++ components/layouts/App.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import Footer from "../Footer";
+import ConfirmationModal from "../modal/confirmation/confirmation_modal";
+import ModalLogin from "../modal/login/login_modal";
 import Navbar from "../Navbar";
 
 const AppLayout = ({ children }) => (
@@ -8,6 +10,8 @@ const AppLayout = ({ children }) => (
       <title>htci.</title>
     </Head>
     <Navbar />
+    <ModalLogin />
+    <ConfirmationModal />
     <div>{children}</div>
     <Footer />
   </>
